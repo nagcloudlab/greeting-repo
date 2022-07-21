805999f4ab755ad217382ae0167fd26da42f8c48
Nagabhushanam <nagcloudlab@gmail.com>
2022-07-21 20:40:19 +0530

    fix

--- greeting-service/src/main/java/com/example/rest/GreetingController.java
+++ greeting-service/src/main/java/com/example/rest/GreetingController.java
@@ -1,22 +1,21 @@
 package com.example.rest;
 
+import com.example.service.GreetingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class GreetingController {
+
+    private final GreetingService greetingService;
 
     @GetMapping(value = "/api/greet/{lang}")
     public ResponseEntity<?> doGreet(@PathVariable String lang) {
-        String message="";
-        if (lang.equals("en")) {
-            message="hello";
-        }
-        if (lang.equals("tn")) {
-            message="vanakkam";
-        }
+        String message= greetingService.getGreetMessage(lang);
         return ResponseEntity.ok(message);
     }
 
---
+++ greeting-service/src/main/java/com/example/service/GreetingService.java
@@ -0,0 +1,24 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class GreetingService {
+
+    private Map<String, String> matrix = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        matrix.put("en", "hello");
+        matrix.put("tn", "vanakkam");
+    }
+
+    public String getGreetMessage(String lang) {
+        return matrix.get(lang);
+    }
+
+}
