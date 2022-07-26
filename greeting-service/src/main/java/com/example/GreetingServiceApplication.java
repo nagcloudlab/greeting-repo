1348103ee2e32bce82ae7ebcfb92a685c6a804f6
Nagabhushanam <nagcloudlab@gmail.com>
2022-07-26 21:04:01 +0530

    updated jenkins file

--- greeting-service/src/main/java/com/example/GreetingServiceApplication.java
+++ greeting-service/src/main/java/com/example/GreetingServiceApplication.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Author : Nag
+// Author : Nagabhushanamn
 
 @SpringBootApplication
 public class GreetingServiceApplication {
