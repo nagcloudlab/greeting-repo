851d098b5483b65e96d57259d9172376a88bdcd2
Nagabhushanam <nagcloudlab@gmail.com>
2022-07-26 20:30:18 +0530

    fix

--- greeting-service/src/main/java/com/example/GreetingServiceApplication.java
+++ greeting-service/src/main/java/com/example/GreetingServiceApplication.java
@@ -3,13 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Nag
+
 @SpringBootApplication
 public class GreetingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GreetingServiceApplication.class, args);
     }
-
-
 
 }
